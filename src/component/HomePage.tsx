12f1b2381cf4890a18893fc4ec0a1d92df11eebd
kashish Vashistha <kashish.vashistha1@gmail.com>
2025-11-30 15:33:47 +0530

    changed size

--- src/component/HomePage.tsx
+++ src/component/HomePage.tsx
@@ -3,48 +3,48 @@ import Image from "next/image";
 function HomePage() {
   return (
     <>
-      <div className="max-w-md mx-auto bg-white shadow-lg rounded-lg border border-gray-200">
+      <div className="w-full sm:max-w-lg mx-auto bg-white shadow-lg rounded-lg border border-gray-200">
         <div className="flex items-center p-4 space-x-4">
           <div className="flex flex-col -space-y-2">
             <div className="flex -space-x-2">
-              <div className="w-12 h-12 rounded-full overflow-hidden border border-gray-300">
+              <div className="w-10 h-10 rounded-full overflow-hidden border border-gray-300">
                 <Image
                   src="/image1.png"
                   alt="File icon"
-                  width={48}
-                  height={48}
+                  width={38}
+                  height={38}
                   className="w-full h-full object-cover"
                 />
               </div>
 
-              <div className="w-12 h-12 rounded-full overflow-hidden border border-gray-300">
+              <div className="w-10 h-10 rounded-full overflow-hidden border border-gray-300">
                 <Image
                   src="/image2.png"
                   alt="Next.js icon"
-                  width={48}
-                  height={48}
+                  width={38}
+                  height={38}
                   className="w-full h-full object-cover"
                 />
               </div>
             </div>
 
             <div className="flex -space-x-2">
-              <div className="w-12 h-12 rounded-full overflow-hidden border border-gray-300">
+              <div className="w-10 h-10 rounded-full overflow-hidden border border-gray-300">
                 <Image
                   src="/image1.png"
                   alt="File icon"
-                  width={48}
-                  height={48}
+                  width={38}
+                  height={38}
                   className="w-full h-full object-cover"
                 />
               </div>
 
-              <div className="w-12 h-12 rounded-full overflow-hidden border border-gray-300">
+              <div className="w-10 h-10 rounded-full overflow-hidden border border-gray-300">
                 <Image
                   src="/image2.png"
                   alt="Next.js icon"
-                  width={48}
-                  height={48}
+                  width={38}
+                  height={38}
                   className="w-full h-full object-cover"
                 />
               </div>
@@ -57,12 +57,12 @@ function HomePage() {
           </div>
 
           <div className="w-10 h-10 flex items-center justify-center bg-red-100 rounded-full border border-red-300">
-            <div className="w-12 h-12 rounded-full overflow-hidden border border-gray-300">
+            <div className="w-10 h-10 rounded-full overflow-hidden border border-gray-300">
               <Image
                 src="/file.svg"
                 alt="File icon"
-                width={48}
-                height={48}
+                width={38}
+                height={38}
                 className="w-full h-full object-cover"
               />
             </div>
