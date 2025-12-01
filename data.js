5856a251c529249c9c34c4d756b307a26d4f015a
kashish Vashistha <kashish.vashistha1@gmail.com>
2025-12-01 21:31:15 +0530

    added logic

---
+++ data.js
@@ -0,0 +1,34 @@
+export const data = [
+  {
+    id: 1,
+    name: "Super",
+    count: 30,
+    images: "/image1.png",
+    ready: true,
+    error: false,
+  },
+  {
+    id: 2,
+    name: "Super",
+    count: 30,
+    images: "/image2.png",
+    ready: true,
+    error: false,
+  },
+  {
+    id: 3,
+    name: "Super",
+    count: 30,
+    images: "/image2.png",
+    ready: false,
+    error: true,
+  },
+  {
+    id: 4,
+    name: "Super",
+    count: 30,
+    images: "/image1.png",
+    ready: false,
+    error: true,
+  },
+];
